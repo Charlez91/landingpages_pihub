429c3fbea420156b6c4fd5b3b03625fbfda42e4c
Charles Okeke <okekecharles91@gmail.com>
2022-07-12 04:54:49 +0100

    First Commit

---
+++ PiHubPro_LandingPage/support/README.md
@@ -0,0 +1,6 @@
+# JavaScript Web Basic Sample
+
+## NOTICE!
+
+This sample is deprecated and not supported any longer.  
+You can see the latest web basic sample in [Sendbird Web Basic Sample](https://github.com/sendbird/SendBird-JavaScript/tree/master/web-basic-sample).
---
+++ PiHub_LandingPage/Pi_Pay/js/main.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){ 
+
+   /*-------------Features Carousel-------------*/
+
+    $('.features-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay:true,
+        responsiveClass:true,
+        responsive:{
+                0:{
+                    items:1,
+                   
+                },
+                600:{
+                    items:2,
+                    
+                },
+                1000:{
+                    items:3,
+                    
+                }
+        }
+    });
+
+    /*-- Toggle Theme Start - light and dark mode--*/
+    function toggleTheme(){
+        if(localStorage.getItem("pihub-theme") !== null){
+             if(localStorage.getItem("pihub-theme") === "dark"){
+                $("body").addClass("dark");
+             }else{
+                $("body").removeClass("dark");
+             }
+        }
+        updateIcon();
+    }
+    toggleTheme();
+    $(".toggle-theme").on("click",function(){
+         $("body").toggleClass("dark");
+         if($("body").hasClass("dark")){
+            localStorage.setItem("pihub-theme","dark");
+         }else{
+            localStorage.setItem("pihub-theme","light");
+         }
+         updateIcon();
+    });
+    function updateIcon(){
+        if($("body").hasClass("dark")){
+            $(".toggle-theme i").removeClass("fa-moon");
+            $(".toggle-theme i").addClass("fa-sun");
+        }else{
+             $(".toggle-theme i").removeClass("fa-sun");
+             $(".toggle-theme i").addClass("fa-moon");
+        }
+    }
+
+});
+
+
+/*FAQs*/
+    const faqs = document.querySelectorAll(".faq");
+
+    faqs.forEach((faq) => {
+        faq.addEventListener("click", () => {
+            faq.classList.toggle("active");
+        });
+    });
+/*end FAQs*/
